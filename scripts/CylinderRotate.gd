08103960b73724a687ea9e782cc7c51f28591921
DeNick <disar2014@gmail.com>
2022-07-06 21:13:39 +0700

    Change scene

--- scripts/CylinderRotate.gd
+++ scripts/CylinderRotate.gd
@@ -3,7 +3,7 @@ extends RigidBody
 
 # Declare member variables here. Examples:
 # var a = 2
-# var b = "text"
+var velocity = 0.4
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,5 +13,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	transform.basis = Basis(Vector3(0, 1, 0), 1) * delta
+	transform.basis = Basis(Vector3(0, 1, 0), (velocity * delta)) * transform.basis 
 	pass
