19573c139d37a6003fb8c154f3e2edc18683d2ac
DeNick <disar2014@gmail.com>
2022-07-08 21:12:07 +0700

    cnange script

--- Lelvels/KnifePhys.gd
+++ Lelvels/KnifePhys.gd
@@ -8,11 +8,13 @@ extends RigidBody
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector3(0,0,10), Vector3(0,0,5));
+	#apply_impulse(Vector3(0,0,10), Vector3(0,0,5));
+	#self.add_force(Vector3(0,0,10), Vector3(0,0,5))
+	self.add_central_force(Vector3(0,0,1000));
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	add_force(Vector3(0,0,10), Vector3(0,0,100 * delta));
-#	pass
+func _process(delta):
+	#self.add_central_force(Vector3(0,0,10 * delta));
+	pass
