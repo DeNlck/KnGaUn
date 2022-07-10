6cb3b14f639138f2305865cc9ce8fbb8077007da
Denis Denis <disar2014@gmail.com>
2022-07-11 00:35:55 +0700

    change script

--- Lelvels/KnifePhys.gd
+++ Lelvels/KnifePhys.gd
@@ -10,11 +10,14 @@ extends RigidBody
 func _ready():
 	#apply_impulse(Vector3(0,0,10), Vector3(0,0,5));
 	#self.add_force(Vector3(0,0,10), Vector3(0,0,5))
-	self.add_central_force(Vector3(0,0,1000));
+	#self.add_central_force(Vector3(0,0,1000));
+	#set_mode(RigidBody.MODE_KINEMATIC);
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#self.add_central_force(Vector3(0,0,10 * delta));
+	#print("We in editor");
+	self.add_central_force(Vector3(0,0,100));
+	#move_and_collide(Vector3(0,0,10), true, false);
 	pass
