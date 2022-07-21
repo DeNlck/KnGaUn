00c13768cbe799ebb74c56a40ee9a204c1ebff82
Denis Denis <disar2014@gmail.com>
2022-07-21 18:56:34 +0700

    something change

--- Lelvels/KnifePhys.gd
+++ Lelvels/KnifePhys.gd
@@ -1,7 +1,7 @@
 extends KinematicBody
 
 
-var isTouched = false
+#var isTouched = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,13 +10,13 @@ func _ready():
 
 #Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if isTouched:
-		get_node("Knife/KnifePhys").move_and_collide(Vector3(0,0,100), true, true, false)
+	#if isTouched:
+		#get_node("Knife/KnifePhys").move_and_collide(Vector3(0,0,100), true, true, false)
 		#get_node("Knife/KnifePhys").add_central_force(Vector3(0,0,100))
 	pass
 	
 func _input(event):
-	if (event.is_pressed() and event.button_index == BUTTON_LEFT):
-		isTouched = true
+#	if (event.is_pressed() and event.button_index == BUTTON_LEFT):
+#		isTouched = true
 	pass
 
--- Lelvels/MainScene.gd
+++ Lelvels/MainScene.gd
@@ -14,10 +14,11 @@ func _ready():
 func _process(delta):
 	if isTouched:
 		colis = get_node("Knife/KnifePhys").move_and_collide(Vector3(0,0,100), true, true, false)
-		if colis:
-			print('Collision info: ' + str(colis.collider.get_property_list()));
+		if colis.collider.name == "CylinderPhys":
+			print('Collision info: ' + str(colis.collider.name));
 			isTouched = false
 			colis = null
+#			get_node("Knife").duplicate(false)
 		#get_node("Knife/KnifePhys").add_central_force(Vector3(0,0,100))
 	pass
 	
