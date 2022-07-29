30cf7f7891d374b98b9071b0aff85b7ea1ee85e3
Denis Denis <disar2014@gmail.com>
2022-07-29 18:00:39 +0700

    edit tree scene and tree folder

--- Classes/Targets/KnifeTarget.gd
+++ Classes/Targets/KnifeTarget.gd
@@ -2,19 +2,7 @@ class_name KnifeTarget
 
 var hp: int
 var isTouched: bool = false
-var privateVar:int setget setPrivatevar, getPrivatevar
 
 func _init(_hp:int = 5):
 	hp = _hp
 	pass
-
-func say(arg):
-	print(arg)
-
-func setPrivatevar(arg:int):
-	printerr(arg)
-	push_warning("test warning")
-	return
-
-func getPrivatevar():
-	return privateVar
