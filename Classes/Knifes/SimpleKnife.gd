1421bf1d695872db6582325d7c7b25c5f229cdbb
Denis Denis <disar2014@gmail.com>
2022-08-09 23:38:03 +0700

    Доработка анимации

--- Classes/Knifes/SimpleKnife.gd
+++ Classes/Knifes/SimpleKnife.gd
@@ -9,9 +9,10 @@ func _ready():
 	pass
 
 func _process(delta):
-	if G.camera_in_gamezone and !check:
+	if G.camera_in_gamezone and !self.check:
 		animPlay_pull_out_knife()
 
 func animPlay_pull_out_knife():
+	self.check = true
+	self.visible = true
 	self.animationController.play("pull_out_knife")
-	check = true
